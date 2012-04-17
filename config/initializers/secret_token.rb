20cf19334aadf8338a8f68ee8c9bc2c22508b698
Dave Green <dave@developerdave.co.uk>
2012-04-17 16:01:23 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CollabATron::Application.config.secret_token = 'd6f917dc5444808f3e8759afeed12e09c45c050015d488f2cb16b7cd136811b9c23359bb64ceedfe2fc3cedfc9d23f049396612d8103e7ed5182ccc6cf3c7232'
