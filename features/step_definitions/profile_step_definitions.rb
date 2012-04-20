ea68cbf5522d440619cba3c2cd4b29aad86c67d4
Dave Green <dave@developerdave.co.uk>
2012-04-20 13:54:54 +0100

    refactored haml and added avatar image

--- features/step_definitions/profile_step_definitions.rb
+++ features/step_definitions/profile_step_definitions.rb
@@ -9,5 +9,6 @@ end
 Then /^I can see the profile avatar$/ do
   within '#profiles' do 
     page.should have_css("a[title='#{@profile.display_name}']")
+    page.should have_css("a img[src='#{@profile.avatar_url}']")
   end
 end
