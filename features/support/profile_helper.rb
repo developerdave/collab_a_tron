290a2a6f6f02057e32204705ee23ec5cfe8b4ec6
Dave Green <dave@developerdave.co.uk>
2012-04-20 16:15:45 +0100

    added styling for wall of avatars

--- features/support/profile_helper.rb
+++ features/support/profile_helper.rb
@@ -1,8 +1,8 @@
 module ProfileHelper
   def avatar_displayed?(profile)
-    within '#profiles' do 
-      page.should have_css("a[title='#{profile.display_name}']")
-      page.should have_css("a img[src='#{profile.avatar_url}']")
+    within '#profiles ul' do 
+      page.should have_css("li a[title='#{profile.display_name}']")
+      page.should have_css("li a img[src='#{profile.avatar_url}']")
     end
   end
 end
