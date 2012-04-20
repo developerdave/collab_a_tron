bb782876970f642566093109a63c25e638d73c6b
Dave Green <dave@developerdave.co.uk>
2012-04-20 09:46:32 +0100

    added view spec

---
+++ spec/views/profile_index_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe "profiles/index" do
+  it "displays all profiles" do
+    assign(:profiles, [
+      stub_model(Profile, :firstname => "Dave"),
+      stub_model(Profile, :firstname => "Tom")
+    ])
+    render
+    rendered.should have_content("Dave")
+  end  
+end
